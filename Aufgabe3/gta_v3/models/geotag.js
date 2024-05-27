e4e4e5568f455a09438372d26707a945c8f6111f
KR-web-dh <mickeymouse20@gmx.de>
2024-05-27 10:27:16 +0000

    Umsetzung geotag class

--- Aufgabe3/gta_v3/models/geotag.js
+++ Aufgabe3/gta_v3/models/geotag.js
@@ -11,7 +11,44 @@
  */
 class GeoTag {
 
-    // TODO: ... your code here ...
+    constructor(name, latitude, longitude, hashtag){
+        this._name = name;
+  	    this._latitude = latitude;
+  	    this._longitude = longitude;
+        this._hashtag = hashtag;
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    setName(name) {
+        this._name = name;
+    }
+
+    getLatitude() {
+        return this._latitude;
+    }
+
+    setLatitude(latitude) {
+        this._latitude = latitude;
+    }
+
+    getLongitude() {
+        return this._longitude;
+    }
+
+    setLongitude(longitude) {
+        this._longitude = longitude;
+    }
+
+    getHashtag(){
+        return this._hashtag
+    }
+
+    setHashtag(hashtag){
+        this._hashtag = hashtag;
+    }
     
 }
 
